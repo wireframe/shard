ecaef256eee3dd31513a54ad1bd764f20b539f4f
wireframe <wireframe>
2004-11-29 03:28:36 +0000

    added spells

---
+++ shard-core/src/java/com/codecrate/shard/magic/Spell.java
@@ -0,0 +1,13 @@
+package com.codecrate.shard.magic;
+
+public class Spell {
+
+	private String school;
+	private String subschool;
+	private boolean spellResistant;
+	
+	//components
+	//range
+	//duration
+	//save
+}
