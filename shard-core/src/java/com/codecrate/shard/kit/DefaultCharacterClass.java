4843770a9bbbc634666ff54ac42db0fc99f28b86
wireframe <wireframe>
2004-11-29 23:11:43 +0000

    moved character class to kit package.

--- shard-core/src/java/com/codecrate/shard/kit/DefaultCharacterClass.java
+++ shard-core/src/java/com/codecrate/shard/kit/DefaultCharacterClass.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package com.codecrate.shard.character;
+package com.codecrate.shard.kit;
 
 import java.util.Collection;
 
