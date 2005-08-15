9b97bd04a11c16204dbbfa3577ff2d96f98c5a64
wireframe <wireframe>
2005-08-15 02:11:06 +0000

    added licenses

--- shard-hibernate/src/java/com/codecrate/shard/lucene/LuceneInterceptor.java
+++ shard-hibernate/src/java/com/codecrate/shard/lucene/LuceneInterceptor.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2004 codecrate consulting
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.codecrate.shard.lucene;
 
 import java.io.IOException;
@@ -31,23 +46,6 @@ public class LuceneInterceptor implements Interceptor {
 		this.analyzer = new StandardAnalyzer();
 	}
 
-    public boolean onLoad(Object entity,
-            Serializable id,
-            Object[] state,
-            String[] propertyNames,
-            Type[] types) {
-		return false;
-	}
-
-    public boolean onFlushDirty(Object entity,
-            Serializable id,
-            Object[] currentState,
-            Object[] previousState,
-            String[] propertyNames,
-            Type[] types) {
-		return false;
-	}
-
 	public boolean onSave(Object entity,
 			Serializable id, Object[] currentState,
 			String[] propertyNames, Type[] types) throws CallbackException {
@@ -72,6 +70,14 @@ public class LuceneInterceptor implements Interceptor {
 		return false;
 	}
 
+    public void onDelete(Object entity,
+            Serializable id,
+            Object[] state,
+            String[] propertyNames,
+            Type[] types) {
+        removeDocuments(id);
+	}
+
 	private void closeWriter(IndexWriter writer) {
 		if (null != writer) {
 		    try {
@@ -80,14 +86,6 @@ public class LuceneInterceptor implements Interceptor {
 				LOG.warn("Error while closing index writer", e);
 			}
 		}
-	}
-
-    public void onDelete(Object entity,
-            Serializable id,
-            Object[] state,
-            String[] propertyNames,
-            Type[] types) {
-        removeDocuments(id);
 	}
 
 	private void removeDocuments(Serializable id) {
@@ -115,6 +113,23 @@ public class LuceneInterceptor implements Interceptor {
 		}
 	}
 
+    public boolean onLoad(Object entity,
+            Serializable id,
+            Object[] state,
+            String[] propertyNames,
+            Type[] types) {
+		return false;
+	}
+
+    public boolean onFlushDirty(Object entity,
+            Serializable id,
+            Object[] currentState,
+            Object[] previousState,
+            String[] propertyNames,
+            Type[] types) {
+		return false;
+	}
+
 	public void preFlush(Iterator entities) throws CallbackException {
 	}
 
@@ -137,5 +152,4 @@ public class LuceneInterceptor implements Interceptor {
 	public Object instantiate(Class entity, Serializable id) throws CallbackException {
 		return null;
 	}
-
 }
--- shard-hibernate/src/java/com/codecrate/shard/lucene/LuceneSearcher.java
+++ shard-hibernate/src/java/com/codecrate/shard/lucene/LuceneSearcher.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2004 codecrate consulting
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.codecrate.shard.lucene;
 
 import java.io.IOException;
@@ -35,19 +50,19 @@ public class LuceneSearcher extends HibernateTemplate {
 		this.directory = directoryManager.getDirectory();
 		this.analyzer = new StandardAnalyzer();
 	}
-	
+
 	public Collection search(Class target, String query) {
 		Searcher searcher = null;
 		Collection results = new ArrayList();
-		
+
 		try {
 			searcher = new IndexSearcher(directory);
 			BooleanQuery masterQuery = new BooleanQuery();
 			masterQuery.add(new TermQuery(new Term(FIELD_CLASS, target.getName())), REQUIRED, NOT_PROHIBITED);
 			masterQuery.add(QueryParser.parse(makeWildcardQuery(query), FIELD_TEXT, analyzer), REQUIRED, NOT_PROHIBITED);
-			
+
 			LOG.info("Searching for " + masterQuery);
-			
+
 			Hits hits = searcher.search(masterQuery);
 			LOG.info("Found " + hits.length() + " matches");
 			for (int x = 0; x < hits.length(); x++) {
@@ -60,14 +75,14 @@ public class LuceneSearcher extends HibernateTemplate {
 					results.add(result);
 				}
 			}
-			
+
 		} catch (Exception e) {
 			LOG.error("Error searching directory " + directory + " for type "
 					+ target + " using query " + query, e);
 		} finally {
 			closeSearcher(searcher);
 		}
-		
+
 		return results;
 	}
 
