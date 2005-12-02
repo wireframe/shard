09e3f9d5497ebafd270626fc4adabf7e96f453d4
wireframe <wireframe>
2005-12-02 05:06:26 +0000

    add initial pcgen import code.

---
+++ shard-transfer/src/main/java/com/codecrate/shard/transfer/pcgen/PcgenSkillImporter.java
@@ -0,0 +1,32 @@
+package com.codecrate.shard.transfer.pcgen;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Collection;
+
+public class PcgenSkillImporter {
+
+    public Collection importSkills(File file) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+
+        while (reader.ready()) {
+            String line = reader.readLine();
+            if (isUsableRow(line)) {
+                System.out.println(line);
+            }
+        }
+
+        reader.close();
+        return null;
+    }
+
+    private boolean isUsableRow(String value) {
+        return (!isEmpty(value) && !value.startsWith("SOURCE"));
+    }
+
+    private boolean isEmpty(String value) {
+        return (null == value || value.trim().length() < 1  || value.startsWith("#"));
+    }
+}
