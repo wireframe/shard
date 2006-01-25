b374626419bfe3d3bb5e68cb8a20936321eb3887
wireframe <wireframe>
2006-01-25 17:07:56 +0000

    add unit test

---
+++ shard-core/src/test/java/com/codecrate/shard/skill/DefaultSkillEntryContainerTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2004 codecrate consulting
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.codecrate.shard.skill;
+
+import java.util.HashMap;
+
+import com.codecrate.shard.modifier.DefaultKeyedModifier;
+import com.codecrate.shard.modifier.DefaultModifierType;
+
+import junit.framework.TestCase;
+
+public class DefaultSkillEntryContainerTest extends TestCase {
+
+	public void testRollSkillCheckSuccess() {
+		DefaultSkill swim = new DefaultSkill("swim", true, null, true, null);
+		DifficultyClass dc = new DifficultyClass(1);
+		DefaultSkillEntryContainer skills = new DefaultSkillEntryContainer(new HashMap(), 1);
+		skills.addModifier(new DefaultKeyedModifier(swim, DefaultModifierType.CLASS, 2));
+		assertTrue(skills.rollSkillCheck(swim, dc));
+	}
+}
