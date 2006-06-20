4cd885892a5070d38808e5ae09dd678194d297dc
wireframe <wireframe>
2006-06-20 02:11:39 +0000

    cleanup code.

--- shard-minotaur/src/main/java/com/codecrate/shard/ShardMinotaurLifecycleAdvisor.java
+++ shard-minotaur/src/main/java/com/codecrate/shard/ShardMinotaurLifecycleAdvisor.java
@@ -10,10 +10,6 @@ public class ShardMinotaurLifecycleAdvisor extends DefaultApplicationLifecycleAd
 private ImportCommand importCommand;
 
 //http://common.l2fprod.com/
-//	public void onPreStartup() {
-//		JOptionPane.showMessageDialog(null, "no objects found", "application error", JOptionPane.WARNING_MESSAGE);
-//	}
-//
 //    public void onPreWindowOpen(ApplicationWindowConfigurer configurer) {
 //        super.onPreWindowOpen(configurer);
 //        if (getApplication().getApplicationContext().containsBean("setupWizard")) {
@@ -29,21 +25,9 @@ private ImportCommand importCommand;
 
     public void onWindowOpened(ApplicationWindow window) {
         super.onWindowOpened(window);
-//        ActionCommand command = window.getCommandManager().getActionCommand("importCommand");
-//        command.execute();
-//    }
-//
-//    public void onPreWindowOpen(ApplicationWindowConfigurer configurer) {
-//        super.onPreWindowOpen(configurer);
         
         importCommand = (ImportCommand) getApplication().getServices().getBean("importCommand", ImportCommand.class);
         importCommand.setApplicationWindow(window);
-    }
-    
-    public void onPostStartup() {
-        super.onPostStartup();
-
         importCommand.execute();
     }
-
 }
