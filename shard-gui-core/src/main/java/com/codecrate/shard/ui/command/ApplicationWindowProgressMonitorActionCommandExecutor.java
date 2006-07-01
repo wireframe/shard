878c5ca8dac8bd142fa6a46991f0574614b7e8c4
wireframe <wireframe>
2006-07-01 22:06:24 +0000

    use bean id to display progress description

--- shard-gui-core/src/main/java/com/codecrate/shard/ui/command/ApplicationWindowProgressMonitorActionCommandExecutor.java
+++ shard-gui-core/src/main/java/com/codecrate/shard/ui/command/ApplicationWindowProgressMonitorActionCommandExecutor.java
@@ -1,8 +1,12 @@
 package com.codecrate.shard.ui.command;
 
 import java.util.Collections;
+import java.util.Locale;
 import java.util.Map;
 
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.MessageSource;
+import org.springframework.context.MessageSourceAware;
 import org.springframework.richclient.application.ApplicationWindow;
 import org.springframework.richclient.application.config.ApplicationWindowAware;
 import org.springframework.richclient.command.ParameterizableActionCommandExecutor;
@@ -10,12 +14,14 @@ import org.springframework.richclient.progress.BusyIndicator;
 import org.springframework.richclient.progress.ProgressMonitor;
 import org.springframework.richclient.progress.StatusBar;
 
-public class ApplicationWindowProgressMonitorActionCommandExecutor implements ParameterizableActionCommandExecutor, ApplicationWindowAware {
+public class ApplicationWindowProgressMonitorActionCommandExecutor implements ParameterizableActionCommandExecutor, ApplicationWindowAware, MessageSourceAware, BeanNameAware {
 	private final ParameterizableActionCommandExecutor delegate;
     private ApplicationWindow window;
+    private MessageSource messageSource;
+    private String messageKey;
 
 	public ApplicationWindowProgressMonitorActionCommandExecutor(ParameterizableActionCommandExecutor delegate) {
-		this.delegate = delegate;
+        this.delegate = delegate;
 	}
 
     private ProgressMonitor getProgressMonitor() {
@@ -26,13 +32,10 @@ public class ApplicationWindowProgressMonitorActionCommandExecutor implements Pa
         execute(Collections.EMPTY_MAP);
     }
 
-    public void setApplicationWindow(ApplicationWindow window) {
-        this.window = window;
-    }
-
     public void execute(Map params) {
+        String description = messageSource.getMessage(messageKey, new Object[] {}, messageKey, Locale.getDefault());
         BusyIndicator.showAt(window.getControl());
-        getProgressMonitor().taskStarted("TEXT GOES HERE!", StatusBar.UNKNOWN);
+        getProgressMonitor().taskStarted(description, StatusBar.UNKNOWN);
 
         try {
             delegate.execute(params);
@@ -40,5 +43,17 @@ public class ApplicationWindowProgressMonitorActionCommandExecutor implements Pa
             BusyIndicator.clearAt(window.getControl());
             getProgressMonitor().done();
         }
+    }
+
+    public void setApplicationWindow(ApplicationWindow window) {
+        this.window = window;
+    }
+
+    public void setMessageSource(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    public void setBeanName(String name) {
+        this.messageKey = name;
     }
 }
