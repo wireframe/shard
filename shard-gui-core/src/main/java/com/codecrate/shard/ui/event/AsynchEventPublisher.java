305bc7c1b85c2b060683d03b9bf37bf574b4f969
wireframe <wireframe>
2006-07-11 21:18:12 +0000

    add asynch event publisher to publish events on background thread.

--- shard-gui-core/src/main/java/com/codecrate/shard/ui/event/AsynchEventPublisher.java
+++ shard-gui-core/src/main/java/com/codecrate/shard/ui/event/AsynchEventPublisher.java
@@ -1,5 +1,7 @@
 package com.codecrate.shard.ui.event;
 
+import javax.swing.SwingUtilities;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.context.ApplicationEvent;
@@ -7,19 +9,20 @@ import org.springframework.context.ApplicationEventPublisher;
 
 public class AsynchEventPublisher implements ApplicationEventPublisher {
 	private static final Log LOG = LogFactory.getLog(AsynchEventPublisher.class);
-	
+
 	private final ApplicationEventPublisher delegate;
 	public AsynchEventPublisher(ApplicationEventPublisher delegate) {
 		this.delegate = delegate;
 
 	}
 	public void publishEvent(final ApplicationEvent event) {
-		LOG.debug("Publishing event on background thread: " + event);
-		new Thread() {
+		LOG.info("Publishing event on background thread: " + event);
+		SwingUtilities.invokeLater(new Runnable() {
+
 			public void run() {
 				delegate.publishEvent(event);
 			}
-		}.run();
+		});
 	}
 
 }
