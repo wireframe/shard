9840e9e6fda356305e9abf99ae3113c9051576a9
wireframe <wireframe>
2006-09-27 13:34:34 +0000

    SHA-132 - add validation for max hit dice per level

--- shard-gui-core/src/main/java/com/codecrate/shard/ui/validation/ShardRulesSource.java
+++ shard-gui-core/src/main/java/com/codecrate/shard/ui/validation/ShardRulesSource.java
@@ -1,6 +1,8 @@
 package com.codecrate.shard.ui.validation;
 
 import org.springframework.rules.Rules;
+import org.springframework.rules.constraint.LessThanEqualTo;
+import org.springframework.rules.constraint.property.PropertiesConstraint;
 import org.springframework.rules.support.DefaultRulesSource;
 
 import com.codecrate.shard.character.DefaultCharacterLevel;
@@ -29,29 +31,7 @@ public class ShardRulesSource extends DefaultRulesSource {
 			protected void initRules() {
 				addRequired("characterClass");
 				add("hitpoints", gt(0));
-//				add("hitpoints", new Constraint() {
-//
-//					public boolean test(Object object) {
-//						Integer value = (Integer) object;
-//						CharacterLevel level = (CharacterLevel) object;
-//						return isHitPointsWithinRange(level);
-//					}
-//
-//					private boolean isHitPointsWithinRange(CharacterLevel level) {
-//						CharacterClass kit = level.getCharacterClass();
-//						if (null == kit) {
-//							return true;
-//						}
-//						if (kit.getHitDicePerLevel().getMinValue() > level.getHitpoints()) {
-//							return false;
-//						}
-//
-//						if (kit.getHitDicePerLevel().getMaxValue() < level.getHitpoints()) {
-//							return false;
-//						}
-//						return true;
-//					}
-//				});
+				add(new PropertiesConstraint("hitpoints", LessThanEqualTo.instance(), "characterClass.hitDicePerLevel.maxValue"));
 			}
 		};
 	}
