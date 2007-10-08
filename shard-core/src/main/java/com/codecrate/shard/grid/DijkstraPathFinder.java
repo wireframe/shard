7cca99ca8a568e4364f7e4acfb5d16b2402ec49c
wireframe <wireframe>
2007-10-08 04:22:13 +0000

    begin adding dijkstra path finder.

---
+++ shard-core/src/main/java/com/codecrate/shard/grid/DijkstraPathFinder.java
@@ -0,0 +1,17 @@
+package com.codecrate.shard.grid;
+
+import java.util.Collection;
+
+/**
+ * 
+ * @see http://renaud.waldura.com/doc/java/dijkstra/
+ * 
+ * @author rsonnek
+ */
+public class DijkstraPathFinder implements PathFinder {
+
+	@Override
+	public Collection<GridSquare> findPathBetween(Grid grid, GridSquare start, GridSquare end) {
+		return null;
+	}
+}
